0444f5952802cdd8361ce80d1e20ea8afdf2c16f
Joze Perez <jozeperezp@gmail.com>
2013-10-06 18:27:34 -0400

    Current site update.

--- scripts/local.compare.js
+++ scripts/local.compare.js
@@ -1,6 +1,37 @@
-$('.container').sameHeight({
-    elements: '.columns',
-    useMinHeight: true,
-    flexible: true,
-    multiLine: true
-});
+// equalize heights helpter utility function
+function equalizeHeights( config ) {
+    // decifer config
+    var selector      = config.selector || false,
+        tallest       = 0;
+
+    // get tallest height
+    $( selector ).each(
+        function( index, el ) {
+            // first reset height real quick
+            $( el ).height( 'auto' );
+            tallest = $( el ).height() > tallest ? $( el ).height() : tallest;
+        }
+    );
+
+    // apply tallest height
+    $( selector ).each(
+        function( index, el ) {
+            $( el ).height( tallest );
+        }
+    );
+}
+
+// equalize everytime window is resized
+$( window ).on( 'resize',
+    function() {
+        // list of selectors to equalize heights
+        var equalizeList = ['.columns h3', '.plan-features'];
+        $.each( equalizeList,
+            function( index, selector ) {
+                equalizeHeights( {
+                    "selector": selector
+                } );
+            }
+        );
+    }
+);
