1e38e3be6c8d3f6f895826f7ef91e9f3d6f7c342
Joze Perez <jozeperezp@gmail.com>
2013-10-20 13:15:32 -0400

    Worked on HLF namespace. Waiting on API.

--- scripts/local.product.js
+++ scripts/local.product.js
@@ -1,77 +1,132 @@
 /**
- * Event listeners
+ * HLF product page
  */
+hlf.register( 'productPage', function() {
+// private data
+    // default config
+    var _config = {
+        apiUrl: '//{SUB-DOMAIN}.healthyleanandfit.com',
+        apiRequiredPreData: 'action=put&method=product_lead&'
+    };
 
-// Green order button, take to form
-$( 'body' ).on( 'click', '.button',
-    function( e ) {
-        var formTarget = $( '.form-headline' );
-        if( formTarget.length ) {
-            $( 'html, body' ).stop().animate( { scrollTop: ( formTarget.offset().top - 80 ) }, { queue:false, duration:1000 } );
-        }
+    // sandbox reference
+    var _sb = null;
 
-        // stop default action
-        e.preventDefault();
-    }
-);
+// private methods
+    // register all listeners for this module
+    var _registerListeners = function() {
+        // green order button, take to form
+        $( '#content-wrapper' ).on( 'click', '.hlf-scroll-to',
+            function( e ) {
+                _sb.util.scrollTo( '.hlf-scroll-to' );
 
-// Form official submit button, send via ajax
-$( 'body' ).on( 'click', '.hlf-submit',
-    function( e ) {
-        // declare variables needed
-        var submitButton       = $( this ),
-            formTarget         = submitButton.closest( 'form' ),
-            loaderSpinner      = formTarget.find( '.loader' ),
-            notificationBanner = formTarget.prev();
-
-        // reset fields & notification banner
-        formTarget.find( '.required' ).removeClass( 'error' ).next().removeClass( 'error' );
-        notificationBanner.fadeOut();
-
-        // loop through data, make sure all required fields are present
-        $( formTarget ).find( '.required' ).each(
-            function( index, originalElement ) {
-                var el = $( originalElement );
-                if( !el.val() || el.val() == originalElement.defaultValue ) {
-                    el.addClass( 'error' ).next().addClass( 'error' );
-                }
+                // stop default action
+                e.preventDefault();
             }
         );
 
-        // show notification banner
-        if( $( '.required.error', formTarget ).size() > 0 ) {
-            $( '.notification' ).fadeIn();
+        // form official submit button, send via ajax
+        $( '#content-wrapper' ).on( 'click', '.hlf-submit',
+            function( e ) {
+                // declare variables needed
+                var submitButton       = $( this ),
+                    formTarget         = submitButton.closest( 'form' ),
+                    loaderSpinner      = formTarget.find( '.loader' ),
+                    notificationBanner = formTarget.prev();
+
+                // reset fields & notification banner
+                formTarget.find( '.required, .is-email' ).removeClass( 'error' ).next().removeClass( 'error' );
+                notificationBanner.fadeOut();
+
+                // loop through data, make sure all required fields are present
+                $( formTarget ).find( '.required' ).each(
+                    function( index, originalElement ) {
+                        var el = $( originalElement );
+                        if( !el.val() || el.val() == originalElement.defaultValue ) {
+                            el.addClass( 'error' ).next().addClass( 'error' );
+                        }
+                    }
+                );
+
+                // loop through any emails
+                $( formTarget ).find( '.is-email' ).each(
+                    function( index, originalElement ) {
+                        var el = $( originalElement );
+                        if( el.val() && el.val() != originalElement.defaultValue && ( el.val().indexOf( '@' ) == -1 || el.val().indexOf( '.' ) == -1 ) ) {
+                            el.addClass( 'error' ).next().addClass( 'error' );
+                        }
+                    }
+                );
+
+                // show notification banner
+                if( $( '.required.error', formTarget ).size() > 0 ) {
+                    $( '.notification' ).fadeIn();
+                }
+                // OR initiate the submission
+                else {
+                    // show spinner & disable submit button
+                    loaderSpinner.fadeIn();
+                    submitButton.attr( 'disabled', 'disabled' );
+
+                    // submit form
+                    $.ajax( {
+                        url: _config.apiUrl,
+                        data: _config.apiRequiredPreData+formTarget.serialize(),
+                        success: function( data ) {
+                            console.log( data );
+                            // success
+                            if( data.requestStatus === 200 ) {
+
+                            }
+                            // error, handle it
+                            else {
+
+                            }
+                        },
+                        error : function( xhr, msg, e ) {
+                            if( window.console ) {
+                                console.error( 'JSONP Error: ', msg, e, xhr );
+                            }
+                        },
+                        dataType: 'jsonp',
+                        cache : false,
+                        crossDomain: true
+                    } );
+                }
+            }
+        );
+    },
+
+    // apply all jquery plugins
+    _applyPlugins = function() {
+        if( $( '.is-telephone' ).size() ) {
+            $( '.is-telephone' ).mask( '(999) 999-9999' );
         }
-        // OR initiate the submission
-        else {
-            // show spinner & disable submit button
-            loaderSpinner.fadeIn();
-            submitButton.attr( 'disabled', 'disabled' );
+    };
 
-            // submit form
-            $.ajax( {
-                url: '//dev.api.healthyleanandfit.com',
-                data: formTarget.serialize(),
-                success: function( data ) {
-                    console.log( data );
-                    // success
-                    if( data.requestStatus === 200 ) {
+// public methods
+    var publik = {};
 
-                    }
-                    // error, handle it
-                    else {
+    // module init method (constructor)
+    publik.init = function(sb) {
+        // set our sandbox Object
+        _sb = sb;
 
-                    }
-                },
-                error : function( xhr, msg, e ) {
-                    if( window.console ) {
-                        console.error( 'JSONP Error: ', msg, e, xhr );
-                    }
-                },
-                dataType: 'jsonp',
-                cache : false,
-                crossDomain: true
-            } );
-        }
-    }
-);
+        // determine what api url to use
+        _config.apiUrl = _config.apiUrl.replace( '{SUB-DOMAIN}', location.host.indexOf( 'dev.' ) > -1 ? 'dev.api' : 'api' );
+
+        // register our listeners
+        _registerListeners();
+
+        // apply our plugins
+        _applyPlugins();
+    };  
+
+    // overwrite _config with config param
+    publik.setConfig = function( config ) {
+        $.extend( true, _config, config );
+    };
+
+    // return our public interface
+    return publik;
+});
